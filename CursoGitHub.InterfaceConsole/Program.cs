27b3f2452e219ae6549a22e93affeaec884236fe
YedoCosta <seu email>
2022-07-04 10:48:51 -0300

    Começando c#

---
+++ CursoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CursoGitHub.InterfaceConsole
+{ //Inicio do namespace
+    internal class Program
+    { // Inicio da classe
+        static void Main(string[] args)
+        { // inicio do método
+            /*
+             * Main - principal
+             * O método inicia-se com a letra maiuscula
+             *
+             */
+
+            Console.WriteLine("Alô Mundo");
+            Console.ReadKey();
+
+
+        } // fin do método
+    } // fim da classe
+} // fim do namespace
